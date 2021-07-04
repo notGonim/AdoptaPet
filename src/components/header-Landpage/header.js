077dc173af3d3902110fc38a8269339cf581586a
notGonim <mhmodgonim96@gmail.com>
2021-07-04 14:00:58 +0200

    ADDED : header for the home page

---
+++ src/components/header-Landpage/header.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+export default function Header() {
+
+
+    return (
+        <header className="h-16  bg-white border-b border-gray-primary  mb-8">
+            <div className="container flex justify-between align-items items-center mx-auto max-w-screen-lg h-full">
+                <div className="">
+                    <img src={process.env.PUBLIC_URL + "favicon.ico"} alt="logo" className="bg-auto bg-no-repeat bg-center " />
+                </div>
+                <div className="flex items-center justify-center align-items  ">
+
+                    <Link to="/" className="font-bold  text-gray-700	text-l   mr-8">   PETS</ Link>
+
+                    <Link to="/" className="font-bold  text-gray-700	text-l   mr-8">   PETS</ Link>
+
+                    <Link to="/" className="font-bold  text-gray-700	text-l   ">   PETS</ Link>
+
+
+                </div>
+                <div className="flex items-center justify-center align-items ">
+                    <button type="button" title="Sign In" className="	text-xl p-2" > Sign in</button>
+                    <button type="button" title="Sign Up" className="bg-blue-800 font-light  text-white	text-xl rounded  m-8 p-2 " > Sign up</button>
+                </div>
+            </div>
+        </header>
+    )
+            }
---
+++ src/pages/LandPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from '../components/header-Landpage/header'
+
+
+export default function LandPage() {
+
+
+    return (
+        <>
+            <Header />
+
+
+        </>
+
+    )
+
+}
