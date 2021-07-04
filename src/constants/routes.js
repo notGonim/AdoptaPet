2d28b02f78a4dfa77c83cee6563bf2621a5fa783
notGonim <mhmodgonim96@gmail.com>
2021-07-04 14:03:32 +0200

    ADDED : constant routes file

---
+++ src/constants/routes.js
@@ -0,0 +1,6 @@
+export const LANDPAGE = '/'
+export const DASHBOARD = '/home'
+export const LOGIN = '/login'
+export const SIGNUP = '/signup'
+export const PROFILE = '/p/:username'
+export const NOT_FOUND = '/not-found'
