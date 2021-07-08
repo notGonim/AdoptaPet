646ec18444646df6cd9e186508e8e98d3444bb0b
notGonim <mhmodgonim96@gmail.com>
2021-07-08 20:36:55 +0200

    Assign the links to its pages

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import man from '../../assets/man.svg'
 
 
@@ -12,7 +13,7 @@ export default function Hero() {
             <div className="text-left m-8 items-start">
                 <h1 className="text-6xl font-medium	text-blue-700 ">For Better Mental Health, Experience The Pet Effect </h1>
                 <p className="text-xlg pt-10 font-medium">Pets, especially dogs and cats, can reduce stress, anxiety, and depression, ease loneliness, encourage exercise and playfulness, and even improve your cardiovascular health. Caring for an animal can help children grow up more secure and active. Pets also provide valuable companionship for older adults</p>
-                <button type="button" className="w-44   bg-blue-700 font-bold  text-white	text-xl rounded  p-3 mt-11 " > Get started</button>
+                <Link to='/signup'  type="button" className="w-44   bg-blue-700 font-bold  text-white text-center	text-xl rounded  p-5 mt-11 " > Get started</Link>
             </div>
 
             <div className="   w-full  ">
