d5e290bae32b45899c81f11ac177de92df5a3965
notGonim <mhmodgonim96@gmail.com>
2021-07-10 05:37:01 +0200

    ADDED the useAuth hook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import * as ROUTES from './constants/routes'
 import { lazy, Suspense } from "react";
+import useAuthListener from "./hooks/useAuth.hook";
+import UserContext from "./context/user.context";
 
 
 
@@ -14,17 +16,22 @@ const Signup = lazy(() => import('./pages/SignupPage'))
 
 
 function App() {
+
+  const { user } = useAuthListener()
+
   return (
-    <Router>
-    <Suspense fallback={<p>Loading ...</p>}>
-      <Switch>
-        <Route  path={ROUTES.LANDPAGE} exact component={Landpage} />
-        <Route  path={ROUTES.LOGIN} exact component={Signin} />
-        <Route  path={ROUTES.SIGNUP} exact component={Signup} />
-      </Switch>
-    </Suspense>
-  </Router>
-  );
+    <UserContext.Provider value={{ user }}>
+      <Router>
+        <Suspense fallback={<p>Loading ...</p>}>
+          <Switch>
+            <Route path={ROUTES.LANDPAGE} exact component={Landpage} />
+            <Route path={ROUTES.LOGIN} exact component={Signin} />
+            <Route path={ROUTES.SIGNUP} exact component={Signup} />
+          </Switch>
+        </Suspense>
+      </Router>
+    </UserContext.Provider>
+  )
 }
 
 export default App;
