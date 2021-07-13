d39e958d9eaa2c50b5251363ca510591e6da0151
notGonim <mhmodgonim96@gmail.com>
2021-07-13 14:15:10 +0200

    ADDED the back to home button

--- src/pages/404.page.js
+++ src/pages/404.page.js
@@ -1,9 +1,11 @@
+import { Link } from 'react-router-dom'
 import error from '../assets/error.svg'
 export default function NotFound() {
 
     return (
-        <div className="conainer    flex justify-center items-center ">
+        <div className="conainer   flex-col flex justify-center items-center ">
             <img src={error} alt="404" className="w-6/12 mt-12" />
+            <Link to='/' className="text-blue-700 m-10" >Back To Home</Link>
         </div>
     )
 }
